33aee86c2a21e3966c888b4ab774f2b139353c98
Petar Mihaylov <petar.m@studio350.com>
2016-07-31 17:27:49 -0400

    Show a randon cat every time

--- js/main.js
+++ js/main.js
@@ -1,13 +1,59 @@
 $("#numberOfCatClicksCat1").prop('disabled', true);
 
 $( function() {
-  var cat1Name = 'Kuddlez';
-  var catCounter1 = $( '#numberOfCatClicksCat1' );
 
-  $('#cat1Name').text(cat1Name);
-  console.log($('#cat1Name'));
+  var cats = {
+    kuddlez: {
+      name: 'Kuddlez',
+      img: 'img/kuddlez.jpg',
+      alt: 'A cuddly cat.'
+    },
+    scaredz: {
+      name: 'Scaredz',
+      img: 'img/scaredz.jpg',
+      alt: 'Scared looking cat.'
+    },
+    napzer: {
+      name: 'Napzer',
+      img: 'img/napzer.jpg',
+      alt: 'A napping cat.'
+    },
+    momzy: {
+      name: 'Momzy',
+      img: 'img/momzy.jpg',
+      alt: 'A cat mommy.'
+    },
+    ninjaz: {
+      name: 'Ninjaz',
+      img: 'img/ninjaz.jpg',
+      alt: 'A ninja kat.'
+    }
+  }
 
-  $( '#cat1' ).on( 'click', function() {
-    catCounter1.val(parseInt(catCounter1.val()) + 1);
+  // Iterate over all the cats so that I can refer to them as numbers
+  var catsArray = [];
+  for (var property in cats) {
+    if (cats.hasOwnProperty(property)) {
+      catsArray.push(property);
+    }
+  }
+
+  // Random integer generator
+  function getRandomIntInclusive(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  // Pick a random currentCat
+  var randomCat = catsArray[getRandomIntInclusive(0,catsArray.length-1)];
+  var currentCat = cats[randomCat];
+
+  $('#catName').text(currentCat.name);
+  $('#catImage').attr('src', currentCat.img);
+  $('#catImage').attr('alt', currentCat.alt);
+
+  var catCounter = $( '#numberOfCatClicksCat' );
+
+  $( '#catImage' ).on( 'click', function() {
+    catCounter.val(parseInt(catCounter.val()) + 1);
   });
 });
