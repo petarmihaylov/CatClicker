450e1a8eb5c5307c06332348e61e74f94b5e4130
Petar Mihaylov <petar.m@studio350.com>
2016-08-28 18:16:32 -0400

    Broke commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $("#numberOfCatClicks").prop('disabled', true);
+//localStorage.clear(); // Remove this line when testing is done so it keeps the click counter
 
 $( function() {
 
@@ -7,10 +8,12 @@ $( function() {
 
   var model = {
     init: function() {
-      var settings = {
-        firstRun: true,
-        lastCatByArrayId: null
-      };
+      var settings = [
+        {
+          firstRun: true,
+          lastCatByArrayId: null
+        }
+      ];
 
       var cats = [
         {
@@ -18,40 +21,39 @@ $( function() {
           img: 'img/kuddlez.jpg',
           alt: 'A cuddly cat.',
           clicks: '0',
-          key: 1
+          id: 1
         },
         {
           name: 'Scaredz',
           img: 'img/scaredz.jpg',
           alt: 'Scared looking cat.',
           clicks: '0',
-          key: 2
+          id: 2
         },
         {
           name: 'Napzer',
           img: 'img/napzer.jpg',
           alt: 'A napping cat.',
           clicks: '0',
-          key: 3
+          id: 3
         },
         {
           name: 'Momzy',
           img: 'img/momzy.jpg',
           alt: 'A cat mommy.',
           clicks: '0',
-          key: 4
+          id: 4
         },
         {
           name: 'Ninjaz',
           img: 'img/ninjaz.jpg',
           alt: 'A ninja kat.',
           clicks: '0',
-          key: 5
+          id: 5
         }
       ];
 
       // Adds the cats to local storage so clicks will be remembered
-      //localStorage.clear(); // Remove this line when testing is done so it keeps the click counter
       if (!localStorage.cats) {
         localStorage.cats = JSON.stringify([]);
         for (var i = 0; i< cats.length; i++) {
@@ -59,16 +61,15 @@ $( function() {
           data.push(cats[i]);
           localStorage.cats = JSON.stringify(data);
         }
-        console.log('localStorage.cats:')
-        console.log(localStorage.cats);
       }
+      // Creates the local storage for the settings
       if (!localStorage.settings) {
         localStorage.settings = JSON.stringify([]);
-        var data = JSON.parse(localStorage.settings);
-        data.push(settings);
-        localStorage.settings = JSON.stringify(data);
-        console.log('localStorage.settings:');
-        console.log(localStorage.settings);
+        for (var i = 0; i< settings.length; i++) {
+          var data = JSON.parse(localStorage.settings);
+          data.push(settings[i]);
+          localStorage.settings = JSON.stringify(data);
+        }
       }
     }, // END: init
 
@@ -81,13 +82,16 @@ $( function() {
       return cats;
     },
 
-    getCatNames: function() {
-      var catNames = [];
-      var data = JSON.parse(localStorage.cats);
+    getSettings: function() {
+      var settings = {};
+      var data = JSON.parse(localStorage.settings);
       for (var i = 0; i < data.length; i++) {
-        catNames.push(data[i].name);
+        $.each(data[i], function(key, value) {
+          settings[key] = value;
+        });
       }
-      return catNames;
+      console.log(settings);
+      return settings;
     }
   } // END: model
 
@@ -103,9 +107,16 @@ $( function() {
     },
 
     render: function() {
-      var catNames = octopus.getCatNames();
-      for (var i = 0; i < catNames.length; i++) {
-        $('#cats').append('<li><h3  id="' + catNames[i] + '">' + catNames[i] + '</h3></li>');
+      var cats = octopus.getCats();
+      //console.log(cats); //This works
+      // Iterate over the cats so that they all get an on-click property to load the cat that was clicked
+      for (var i = 0; i < cats.length; i++) {
+        $('#cats').append('<li><h3  id="' + 'cat-' + cats[i].id + '">' + cats[i].name + '</h3></li>');
+        $('#cat-' + cats[i].id).on('click', (function(cat) {
+          return function(){
+            octopus.showCat(cat);
+          };
+        })(cats[i]));
       }
     }
   };
@@ -116,13 +127,37 @@ $( function() {
   /******* START: VIEW - CAT VIEWER *******
   ****************************************/
   var viewCatViewer = {
-    init: function(cat) {
+    initFirstRun: function () {
+      var cat = octopus.getRandomCat();
+      //console.log(cat); //This works
       viewCatViewer.render(cat);
     },
+
+    init: function() {
+      var cat = octopus.getLastCatViewed();
+      viewCatViewer.render(cat);
+    },
+
+    // Adds onClick functions to all cat images - when in shouldn't!
+    bootstrap: function() {
+      var cats = octopus.getCats();
+      for (var i = 0; i < cats.length; i++) {
+        $('#catImage-' + cats[i].id).on('click', (function(cat) {
+          return function() {
+            octopus.updateClickCount(cat);
+          };
+        })(cats[i]));
+      }
+    },
+
     render: function(cat) {
+      var catImage = $('.catImage');
       $('#catName').text(cat.name);
-      $('#catImage').attr('src', cat.img);
-      $('#catImage').attr('alt', cat.alt);
+      catImage.attr('src', cat.img);
+      catImage.attr('alt', cat.alt);
+      catImage.attr('id', 'catImage-' + cat.id);
+      viewCatViewer.bootstrap();
+      octopus.updateLastCatViewed(cat);
     }
   };
 
@@ -138,30 +173,19 @@ $( function() {
     init: function() {
       model.init();
       viewMenu.init();
-      var settings = JSON.parse(localStorage.settings);
-      console.log(settings);
-      if (settings[0].firstRun) {
-        var catData = this.getRandomCat();
-        var cat = catData[0];
-        viewCatViewer.init(cat);
-        // Update the settings
-        settings[0].firstRun = false;
-        settings[0].lastCatByArrayId = catData[1];
+      var settings = octopus.getSettings();
+      if (settings.firstRun) {
+        // Load this as the first run to get a random cat
+        viewCatViewer.initFirstRun();
+        // Update the settings to indicate the aplication has been run before
+        settings.firstRun = false;
         localStorage.settings = JSON.stringify(settings);
-        console.log('After updating the settings:');
-        console.log(localStorage.settings);
       } else {
-        console.log('settings[0].firstRun:');
-        console.log(settings[0].firstRun);
-        var catId = settings[0].lastCatByArrayId
+        var catId = settings.lastCatByArrayId;
         var cat = this.getCatById(catId);
-        console.log(cat);
-        viewCatViewer.init(cat);
+        // Load the regular init so that the last cat viewed shows up
+        viewCatViewer.init();
       }
-    },
-
-    getCatNames: function() {
-      return model.getCatNames();
     },
 
     getRandomNumber: function(min, max) {
@@ -174,14 +198,42 @@ $( function() {
 
     getCatById: function(catId) {
       var cats = this.getCats();
-      return cats[catId]
+      return cats[catId-1];
     },
 
     getRandomCat: function() {
       var cats = octopus.getCats();
       var catId = octopus.getRandomNumber(0, cats.length -1);
       var randomCat = cats[catId];
-      return [randomCat, catId]
+      return randomCat;
+    },
+
+    getLastCatViewed: function() {
+      var settings = JSON.parse(localStorage.settings);
+      var catId = settings.lastCatByArrayId
+      var cat = this.getCatById(catId);
+      localStorage.settings = JSON.stringify(settings);
+      return cat;
+    },
+
+    getSettings: function() {
+      return model.getSettings();
+    },
+
+    showCat: function(cat) {
+      viewCatViewer.render(cat);
+    },
+
+    updateClickCount: function(cat) {
+      // do stuff to update the click count
+      alert(cat.name);
+    },
+
+    updateLastCatViewed: function(cat) {
+      var settings = octopus.getSettings();
+      settings.lastCatByArrayId = cat.id;
+      settings.firstRun = false;
+      localStorage.settings = JSON.stringify(settings);
     }
   };
 
